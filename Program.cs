867b0e9d235ad864ef060155f98b363f14f818ef
AlXstorCode <owhenpadilla.1408@gmail.com>
2026-09-12 14:19:31 -0500

    Agregar primer modulo: LeerEntero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿string productos;
+int opt = 0;
+static int LeerEntero(string mensaje, int min, int max)
+{
+    int numero = 0;
+    while (true)
+    {
+        Console.WriteLine(mensaje);
+        string entrada = Console.ReadLine();
+
+        if (!int.TryParse(entrada, out numero))
+        {
+            Console.WriteLine("[ERROR] Entrada no valida. Debe ingresar un numero entero.");
+            continue;
+        }
+        if (numero < min || numero > max)
+        {
+            Console.WriteLine($"[ERROR] El numero debe estar entre {min} y {max}");
+            continue;
+        }
+        return numero;
+    }
+}
+
+do
+{
+    Console.WriteLine("SISTEMA GESTOR DE VENTAS E INVESTARIO (MINI-POS)");
+    Console.WriteLine("1. Registrar nuevo producto en inventario");
+    Console.WriteLine("2. Consultar inventario completo");
+    Console.WriteLine("3. Registrar una venta");
+    Console.WriteLine("4. Ver reporte de caja y estadísticas diarias");
+    Console.WriteLine("5. Salir");
+    Console.Write("Seleccione una opción (1-5): ");
+  
+
+
+} while (opt !=5);
